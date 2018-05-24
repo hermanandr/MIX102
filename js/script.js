124a710d3a950034b34927ccd7469ef6393e6a7b
Herman <hermanandr@gmail.com>
2018-05-24 15:16:42 +0200

    parrall

--- js/script.js
+++ js/script.js
@@ -4,17 +4,22 @@ var container = document.getElementById('container');
 var windowHeight = window.innerHeight;
 var windowWidth = window.innerWidth;
 var scrollArea = 1000 - windowHeight;
-var square1 = document.getElementById('en');
-var square2 = document.getElementById('to');
+
+var title = document.getElementById('title');
+var titleHeight = document.getElementById('title').style.top;
+/* var square1 = document.getElementById('en');
+var square2 = document.getElementById('to'); */
 
 // update position of square 1 and square 2 when scroll event fires.
 window.addEventListener('scroll', function() {
 
-  var scrollTop = window.pageYOffset || window.scrollTop;
-  var scrollPercent = scrollTop/scrollArea || 0;
-  
-    square1.style.top = scrollPercent*window.innerHeight + 'px';
+    var scrollTop = window.pageYOffset || window.scrollTop;
+    var scrollPercent = scrollTop/scrollArea || 0;
 
-  /* square1.style.left = scrollPercent*window.innerWidth + 'px';
-  square2.style.left = 800 - scrollPercent*window.innerWidth*0.6 + 'px'; */
+    title.style.top = 600 - scrollPercent*window.innerHeight + 'px';
+
+    /*
+    square1.style.top = scrollPercent*window.innerHeight + 'px';
+    square1.style.left = scrollPercent*window.innerWidth + 'px';
+    square2.style.left = 800 - scrollPercent*window.innerWidth*0.6 + 'px'; */
 });
